fe26b464ed2d535c109d91a3d3324aab4b10fccb
krishnakalyanax <krishnakalyanax@gmail.com>
2024-05-25 20:56:45 +0530

    mobile nav added

---
+++ components/common/MobileNav.tsx
@@ -0,0 +1,76 @@
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+import { RxHamburgerMenu } from 'react-icons/rx'
+import ModeToggle from './ThemeToggle'
+import Link from 'next/link'
+import { routeConstant } from '@/lib/constants/routeConstant'
+export default function MobileNav() {
+  return (
+    <div className='flex bg-accent justify-between p-2'>
+      <ModeToggle />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant='ghost' size={'icon'}>
+            <RxHamburgerMenu />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className='w-56'>
+          <DropdownMenuLabel>QANDA</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <Link
+              href={{
+                pathname: `${routeConstant.HOME}`
+              }}
+            >
+              <DropdownMenuItem>Home</DropdownMenuItem>
+            </Link>
+            <Link
+              href={{
+                pathname: `${routeConstant.HOME}`
+              }}
+            >
+              <DropdownMenuItem>Trending</DropdownMenuItem>
+            </Link>
+            <Link
+              href={{
+                pathname: `${routeConstant.PROFILE}`
+              }}
+            >
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+            </Link>
+            <Link
+              href={{
+                pathname: `${routeConstant.REGISTER}`
+              }}
+            >
+              <DropdownMenuItem>Register</DropdownMenuItem>
+            </Link>
+            <Link
+              href={{
+                pathname: `${routeConstant.LOGIN}`
+              }}
+            >
+              <DropdownMenuItem>Login</DropdownMenuItem>
+            </Link>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Log out</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  )
+}
--- components/common/layouts/MainLayout.tsx
+++ components/common/layouts/MainLayout.tsx
@@ -1,11 +1,15 @@
 import { ReactNode } from 'react'
 import Navbar from '../Navbar'
+import MobileNav from '../MobileNav'
 
 export default function MainLayout({
   children
 }: Readonly<{ children: ReactNode }>) {
   return (
-    <div className='flex h-full'>
+    <div className='flex h-full flex-col md:flex-row'>
+      <div className='block md:hidden'>
+        <MobileNav />
+      </div>
       <div className='flex-1 overflow-auto p-4'>{children}</div>
       <div className='hidden md:block'>
         <Navbar />
